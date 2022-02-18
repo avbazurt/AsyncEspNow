fb63f5ac75ad016ce9232242802c4e7a09e85507
avbazurt <avbazurt@espol.edu.ec>
2022-02-18 07:27:01 -0500

    Agregar Semaforos Envio datos

--- src/AsyncEspNow.cpp
+++ src/AsyncEspNow.cpp
@@ -1,142 +1,194 @@
 #include "AsyncEspNow.h"
 
 
+#if CONFIG_IDF_TARGET_ESP32
+#define CORE_ESP    0
+
+#elif CONFIG_IDF_TARGET_ESP32S2
+#define CORE_ESP    1
+
+#else
+#error CORE no disponible para board objetivo!
+#endif
+
+
+//Puntero Callback
 void (*punteroCallback)(char MAC[], char text[]);
 bool status_send;
 
+
+//Semaphoro
+SemaphoreHandle_t SendDataSemaphore = xSemaphoreCreateCounting( 1, 0 );
+
+
+
+AsyncEspNow::AsyncEspNow(String name)
+{
+  //Guardo el nombre a utilizar
+  nameGroup = name;
+
+  WiFi.mode(WIFI_AP_STA);
+
+  String MAC = WiFi.macAddress();
+  log_i("My MAC Address is: %s", MAC.c_str());
+  status_send = false;
+
+  //Configuramos el nombre de la red
+  MAC.replace(":", "");
+
+  String name_mac = "{NAME}-{MAC}";
+
+  name_mac.replace("{NAME}", name);
+  name_mac.replace("{MAC}", MAC);
+
+  WiFi.softAP(name_mac.c_str());
+}
+
+
 void AsyncEspNow::formatMacAddress(const uint8_t *macAddr, char *buffer, int maxLength)
 {
-    snprintf(buffer, maxLength, "%02x:%02x:%02x:%02x:%02x:%02x", macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
+  snprintf(buffer, maxLength, "%02x:%02x:%02x:%02x:%02x:%02x", macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
 }
 
-void AsyncEspNow::receiveCallback(const uint8_t *macAddr, const uint8_t *data, int dataLen)
-{
 
-    // only allow a maximum of 250 characters in the message + a null terminating byte
-    char buffer[ESP_NOW_MAX_DATA_LEN + 1];
-    int msgLen = min(ESP_NOW_MAX_DATA_LEN, dataLen);
-    strncpy(buffer, (const char *)data, msgLen);
-    // make sure we are null terminated
-    buffer[msgLen] = 0;
-    // format the mac address
-    char macStr[18];
-    formatMacAddress(macAddr, macStr, 18);
-    // debug log the message to the serial port
-    punteroCallback(macStr, buffer);
-}
+/*------------------------------------------------------------------------------------------------------*/
+/*---------------------------------------------- SEND DATA ---------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------*/
+
 
 void AsyncEspNow::sentCallback(const uint8_t *macAddr, esp_now_send_status_t status)
 {
 
-    char macStr[18];
-    formatMacAddress(macAddr, macStr, 18);
+  char macStr[18];
+  formatMacAddress(macAddr, macStr, 18);
 
-    status_send = status == ESP_NOW_SEND_SUCCESS ? true : false;
+  status_send = status == ESP_NOW_SEND_SUCCESS ? true : false;
 
-    log_i("Last Packet Sent to: %s", macStr);
-    log_i("Last Packet Send Status: %s", status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  log_i("Last Packet Sent to: %s", macStr);
+  log_i("Last Packet Send Status: %s", status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 
-    // return status == ESP_NOW_SEND_SUCCESS;
+  log_i("Se libera el semaphore");
+  xSemaphoreGive(SendDataSemaphore);
+
+  //TimerHandle_t _timer_activarMezclador;
+  //AsyncEspNow->SendDataSemaphore;
+  //AsyncEspNow *self = (AsyncEspNow *)pvTimerGetTimerID(_timer_activarMezclador);
+  //AsyncEspNow *self = (AsyncEspNow *);
+
+  //xSemaphoreGive((AsyncEspNow *).SendDataSemaphore);
+
 }
+
+
+bool AsyncEspNow::sentData(uint8_t peerAddress[], const String &message)
+{
+  //Copiamos la informacion
+  memcpy(_peerAddressSend, peerAddress, 6);
+  _message_send = message;
+
+  //Iniciamos la tarea
+  log_i("Iniciar RTOS");
+  xTaskCreatePinnedToCore(
+    this->_task_sendData,        //Function to implement the task
+    "httpsTask",            //Name of the task
+    5000,                   //Stack size in words
+    this,                   //Task input parameter
+    2,                      //Priority of the task
+    &TaskHandSendData,      //Task handle.
+    CORE_ESP);              //Core where the task should run
+
+  log_i("Inicia el semaphore esperando");
+  xSemaphoreTake(SendDataSemaphore, portMAX_DELAY);
+  log_i("Se termina la tarea");
+  return status_send;
+}
+
+
+void AsyncEspNow::_task_sendData(void *pvParameters) {
+  //Variable que toma todas las demas variables
+
+  AsyncEspNow *l_pThis = (AsyncEspNow *) pvParameters;
+
+
+  esp_now_peer_info_t peerInfo = {};
+  memcpy(&peerInfo.peer_addr, l_pThis->_peerAddressSend, 6);
+
+
+  if (!esp_now_is_peer_exist(l_pThis->_peerAddressSend))
+  {
+    esp_now_add_peer(&peerInfo);
+  }
+
+  String message = l_pThis->_message_send;
+
+  esp_err_t result = esp_now_send(l_pThis->_peerAddressSend, (const uint8_t *)message.c_str(), message.length());
+  vTaskDelay(500);  // one tick delay (15ms) in between reads for stability
+  log_i("Se envio Data");
+
+  vTaskDelay(100);  // one tick delay (15ms) in between reads for stability
+  vTaskDelete(l_pThis->TaskHandSendData);
+}
+
+
+
+/*-------------------------------------------------------------------------------------------------------*/
+/*---------------------------------------------- RECIVE DATA ---------------------------------------------*/
+/*-------------------------------------------------------------------------------------------------------*/
+
+
+
+void AsyncEspNow::receiveCallback(const uint8_t *macAddr, const uint8_t *data, int dataLen)
+{
+  // only allow a maximum of 250 characters in the message + a null terminating byte
+  char buffer[ESP_NOW_MAX_DATA_LEN + 1];
+  int msgLen = min(ESP_NOW_MAX_DATA_LEN, dataLen);
+  strncpy(buffer, (const char *)data, msgLen);
+  // make sure we are null terminated
+  buffer[msgLen] = 0;
+  // format the mac address
+  char macStr[18];
+  formatMacAddress(macAddr, macStr, 18);
+
+
+  // debug log the message to the serial port
+  punteroCallback(macStr, buffer);
+}
+
+
 
 void AsyncEspNow::setReciveCallback(void (*puntero)(char MAC[], char text[]))
 {
-    punteroCallback = puntero;
+  punteroCallback = puntero;
 }
 
-AsyncEspNow::AsyncEspNow(String name)
-{
-    //Guardo el nombre a utilizar
-    nameGroup = name;
 
-    WiFi.mode(WIFI_AP_STA);
-    
-    String MAC = WiFi.macAddress();
-    log_i("My MAC Address is: %s", MAC.c_str());
-    status_send = false;
+/*-------------------------------------------------------*/
+/*---------------------- RECIVE DATA ---------------------*/
+/*--------------------------------------------------------*/
 
-    //Configuramos el nombre de la red
-    MAC.replace(":", "");
-
-    String name_mac = "{NAME}-{MAC}";
-
-    name_mac.replace("{NAME}",name);
-    name_mac.replace("{MAC}", MAC);
-
-    WiFi.softAP(name_mac.c_str());
-
-}
 
 void AsyncEspNow::begin()
 {
-    if (esp_now_init() == ESP_OK)
-    {
-        log_i("ESPNow Init Success");
-    }
-    else
-    {
-        log_e("ESPNow Init Failed");
-        delay(3000);
-        ESP.restart();
-    }
+  if (esp_now_init() == ESP_OK)
+  {
+    log_i("ESPNow Init Success");
+  }
+  else
+  {
+    log_e("ESPNow Init Failed");
+    delay(3000);
+    ESP.restart();
+  }
 
-    esp_now_register_send_cb(sentCallback);
-    esp_now_register_recv_cb(receiveCallback);
+  esp_now_register_send_cb(sentCallback);
+  //esp_now_register_recv_cb(receiveCallback);
+
 }
 
-bool AsyncEspNow::sentData(uint8_t peerAddress[], const String &message, bool validate_send)
-{
-    // and this will send a message to a specific device
-    esp_now_peer_info_t peerInfo = {};
-    memcpy(&peerInfo.peer_addr, peerAddress, 6);
-    
-    if (!esp_now_is_peer_exist(peerAddress))
-    {
-        esp_now_add_peer(&peerInfo);
-    }
-    esp_err_t result = esp_now_send(peerAddress, (const uint8_t *)message.c_str(), message.length());
-    if (validate_send)
-    {
-        vTaskDelay(75 / portTICK_PERIOD_MS);
-    }
-    return status_send;
-    /*
-    if (result == ESP_OK)
-    {
-        Serial.println("Broadcast message success");
-        Serial.println("");
-        return true;
-    }
-    else
-    {
-        if (result == ESP_ERR_ESPNOW_NOT_INIT)
-        {
-            Serial.println("ESPNOW not Init.");
-        }
-        else if (result == ESP_ERR_ESPNOW_ARG)
-        {
-            Serial.println("Invalid Argument");
-        }
-        else if (result == ESP_ERR_ESPNOW_INTERNAL)
-        {
-            Serial.println("Internal Error");
-        }
-        else if (result == ESP_ERR_ESPNOW_NO_MEM)
-        {
-            Serial.println("ESP_ERR_ESPNOW_NO_MEM");
-        }
-        else if (result == ESP_ERR_ESPNOW_NOT_FOUND)
-        {
-            Serial.println("Peer not found.");
-        }
-        else
-        {
-            Serial.println("Unknown error");
-        }
-        return false;
-    }
-    */
-}
+
+
+
+
 
 // Global copy of slave
 #define NUMSLAVES 20
@@ -144,87 +196,73 @@ esp_now_peer_info_t slaves[NUMSLAVES] = {};
 int SlaveCnt = 0;
 
 #define CHANNEL 3
-#define PRINTSCANRESULTS 0
 
 // Scan for slaves in AP mode
 void AsyncEspNow::ScanForSlave()
 {
-    int8_t scanResults = WiFi.scanNetworks();
-    // reset slaves
-    memset(slaves, 0, sizeof(slaves));
-    SlaveCnt = 0;
-    Serial.println("");
-    if (scanResults == 0)
+  //Escaneo los resultados
+  int8_t scanResults = WiFi.scanNetworks();
+
+  // reset slaves
+  memset(slaves, 0, sizeof(slaves));
+  SlaveCnt = 0;
+
+  Serial.println("");
+  if (scanResults == 0)
+  {
+    Serial.println("No WiFi devices in AP Mode found");
+  }
+  else
+  {
+    for (int i = 0; i < scanResults; ++i)
     {
-        Serial.println("No WiFi devices in AP Mode found");
-    }
-    else
-    {
-        Serial.print("Found ");
-        Serial.print(scanResults);
-        Serial.println(" devices ");
-        for (int i = 0; i < scanResults; ++i)
+      // Print SSID and RSSI for each device found
+      String SSID = WiFi.SSID(i);
+      int32_t RSSI = WiFi.RSSI(i);
+      String BSSIDstr = WiFi.BSSIDstr(i);
+
+
+      // Check if the current device starts with `Slave`
+      if (SSID.indexOf("ESP-") == 0)
+      {
+        // SSID of interest
+        Serial.print(i + 1);
+        Serial.print(": ");
+        Serial.print(SSID);
+        Serial.print(" [");
+        Serial.print(BSSIDstr);
+        Serial.print("]");
+        Serial.print(" (");
+        Serial.print(RSSI);
+        Serial.print(")");
+        Serial.println("");
+        // Get BSSID => Mac Address of the Slave
+        int mac[6];
+
+        if (6 == sscanf(BSSIDstr.c_str(), "%x:%x:%x:%x:%x:%x", &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5]))
         {
-            // Print SSID and RSSI for each device found
-            String SSID = WiFi.SSID(i);
-            int32_t RSSI = WiFi.RSSI(i);
-            String BSSIDstr = WiFi.BSSIDstr(i);
-
-            if (PRINTSCANRESULTS)
-            {
-                Serial.print(i + 1);
-                Serial.print(": ");
-                Serial.print(SSID);
-                Serial.print(" [");
-                Serial.print(BSSIDstr);
-                Serial.print("]");
-                Serial.print(" (");
-                Serial.print(RSSI);
-                Serial.print(")");
-                Serial.println("");
-            }
-            delay(10);
-            // Check if the current device starts with `Slave`
-            if (SSID.indexOf("ESP") == 0)
-            {
-                // SSID of interest
-                Serial.print(i + 1);
-                Serial.print(": ");
-                Serial.print(SSID);
-                Serial.print(" [");
-                Serial.print(BSSIDstr);
-                Serial.print("]");
-                Serial.print(" (");
-                Serial.print(RSSI);
-                Serial.print(")");
-                Serial.println("");
-                // Get BSSID => Mac Address of the Slave
-                int mac[6];
-
-                if (6 == sscanf(BSSIDstr.c_str(), "%x:%x:%x:%x:%x:%x", &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5]))
-                {
-                    for (int ii = 0; ii < 6; ++ii)
-                    {
-                        slaves[SlaveCnt].peer_addr[ii] = (uint8_t)mac[ii];
-                    }
-                }
-                slaves[SlaveCnt].channel = CHANNEL; // pick a channel
-                slaves[SlaveCnt].encrypt = 0;       // no encryption
-                SlaveCnt++;
-            }
+          for (int ii = 0; ii < 6; ++ii)
+          {
+            slaves[SlaveCnt].peer_addr[ii] = (uint8_t)mac[ii];
+          }
         }
+        slaves[SlaveCnt].channel = CHANNEL; // pick a channel
+        slaves[SlaveCnt].encrypt = 0;       // no encryption
+        SlaveCnt++;
+      }
     }
+  }
 
-    if (SlaveCnt > 0)
-    {
-        Serial.print(SlaveCnt);
-        Serial.println(" Slave(s) found, processing..");
-    }
-    else
-    {
-        Serial.println("No Slave Found, trying again.");
-    }
+  if (SlaveCnt > 0)
+  {
+    Serial.print(SlaveCnt);
+    Serial.println(" Slave(s) found, processing..");
+  }
+  else
+  {
+    Serial.println("No Slave Found, trying again.");
+  }
 
-    // clean up ram
-    WiFi.scanDelete();
+  // clean up ram
+  WiFi.scanDelete();
 }
--- src/AsyncEspNow.h
+++ src/AsyncEspNow.h
@@ -7,22 +7,45 @@
 class AsyncEspNow
 {
 private:
+    //Variable guardar name
+    String nameGroup;
+
+
+    //Variables a utilizar en Send Data
+    uint8_t _peerAddressSend[6];
+    String  _message_send;
+
+
+    //Semaphoros a Utilizar
+
+
+    //Timer para control de las tareas
+    TaskHandle_t TaskHandSendData = NULL;
+
+
+    // Callbacks de timers - NO LLAMAR DIRECTAMENTE
+    static void _task_sendData(void *pvParameters);
+
+    
+public:
+    //Constructor
+    AsyncEspNow(String name = "ESP");
+
+    //Funcion para iniciar ESPnow
+    void begin();
+
+    //Funcion para designar la funcion CallBack
+    static void setReciveCallback(void (*puntero)(char MAC[], char text[])); 
+
+    esp_now_peer_info_t *dispositivos;
+
+    bool sentData(uint8_t peerAddress[], const String &message);
+    void ScanForSlave();
+
+    //Callback
     static void formatMacAddress(const uint8_t *macAddr, char *buffer, int maxLength);
     static void receiveCallback(const uint8_t *macAddr, const uint8_t *data, int dataLen);
     static void sentCallback(const uint8_t *macAddr, esp_now_send_status_t status);
-    
-public:
-    esp_now_peer_info_t *dispositivos;
 
-
-    String nameGroup;
-
-    AsyncEspNow(String name = "ESP");
-    void begin();
-
-    static void setReciveCallback(void (*puntero)(char MAC[], char text[])); 
-    bool sentData(uint8_t peerAddress[], const String &message, bool validate_send = false);
-
-    void ScanForSlave();
 
 };
